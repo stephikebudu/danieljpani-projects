14229b7ac93063e81bd6ccb3ac684722ca681b59
Stephany Ikebudu <stephikebudu@gmail.com>
2026-02-18 12:20:29 +0100

    doc: month 2, week 1 project

---
+++ month-2/week-1/WEEK-1-README.md
@@ -0,0 +1,17 @@
+# Month 2, Week 1 Project
+## Objective
+Build logic to solidify understanding of these JavaScript concepts: Variables and Data types (including Array & Object), Data Manipulation for different typees of data, Loops and Conditionals.
+
+## Project
+
+Title: Grocery Store Inventory Management
+Challenge: Given an the store's inventory which is an array of objects, you are to process the data to answer the following questions as the manager:
+
+1. Task 1 - What items are out of stock?
+2. Task 2 - What items are on discount?
+
+## Guide
+- Do not create a new file, see index.js file in `month-2/week-1/` folder.
+- Follow the instructions in the file to come up with a solution. You will defend your solution, ensure it is yours.
+- Run this command: `node month-2/week-1/index.js` in your terminal to see `console.log()` outputs which is the result of calling your function.
+
---
+++ month-2/week-1/index.js
@@ -0,0 +1,40 @@
+// The store's inventory
+const inventory = [
+  { name: "Apple", category: "Fruit", price: 1.5, quantity: 12, organic: true },
+  { name: "Banana", category: "Fruit", price: 0.8, quantity: 0, organic: false },
+  { name: "Broccoli", category: "Vegetable", price: 2.0, quantity: 5, organic: true },
+  { name: "Milk", category: "Dairy", price: 3.5, quantity: 2, organic: false },
+  { name: "Cheddar", category: "Dairy", price: 5.0, quantity: 8, organic: false },
+  { name: "Spinach", category: "Vegetable", price: 1.2, quantity: 0, organic: true }
+];
+
+
+
+// Task 1 - What items are out of stock?
+function isAvailable() {
+  let outOfStock = [];
+
+  // Write code below this line: Loop through inventory and check if quantity in each item in inventory is 0. If yes, add item to outOfStock array created above
+
+
+
+
+  return outOfStock;
+}
+
+console.log(isAvailable());
+
+
+// Task 2 - What items are on discount?
+function isOnDiscount() {
+  let discountedItems = [];
+
+  // Write code below this line: Loop through inventory and check if price of each item in inventory is greater than 3. If yes, add item to discountedItems array created above
+
+
+
+
+  return discountedItems;
+}
+
+console.log(isOnDiscount());
