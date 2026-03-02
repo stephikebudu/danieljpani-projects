6e8c91355a750fe3bd556d1729a9b5e1c4e60fbc
Stephany Ikebudu <stephikebudu@gmail.com>
2026-03-02 11:53:05 +0100

    month 2, week 2 project

---
+++ month-2/week-2/WEEK-2-README.md
@@ -0,0 +1,15 @@
+# Month 2, Week 2 Project
+## Objective
+Build logic to solidify understanding of these JavaScript concepts: Functions, Callback Functions, Arrow Functions and Scope.
+
+## Project
+Title: Home Automation System
+Challenge: Given a list of devices in a smart home, use code to:
+
+Task 1 - Check device type.
+Task 2 - Check for devices that are turned on.
+Task 3 - Explain the scope of variable in yoiur code.
+
+## Guide
+- Do not create a new file, see index.js file in `month-2/week-2/` folder.
+- Follow the instructions in the file to come up with a solution. You will defend your solution, ensure it is yours.
---
+++ month-2/week-2/index.js
@@ -0,0 +1,26 @@
+// Smart devices in home
+const devices = [
+  { name: "Living Room Light", type: "light", isOn: false },
+  { name: "Air Conditioner", type: "ac", isOn: true },
+  { name: "Front Door Lock", type: "security", isOn: true }
+];
+
+let activeDevices = [];
+
+// Task 1 - Check device type
+/*
+ * Write a regular function called deviceType. It should take a parameter: device (an object) and display the type (property) of the device (object) in the console.
+*/
+
+
+
+
+// Task 2 - Check for devices that are on
+/*
+ * Write an arrow function and assign it to activeDevices. It should take devices array as a parameter and filter out devices that are off (use filter() callback function).
+*/
+
+// Task 3 - Why can you update activeDevices variable from within your function.
+/*
+* Answer: 
+*/
